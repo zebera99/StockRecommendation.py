48e749724b2025781b543ff13976ab06c4bb5cb6
SUNG MIN <76399201+zebera99@users.noreply.github.com>
2022-12-01 11:11:58 +0000

    edited defensive universe to percentage, added code printing out three biggest defensive universe when canary rule doesn't meet. Also added if the current price/12 month average is lower than 1, then print cash

--- main.py
+++ main.py
@@ -69,13 +69,22 @@ LQD_12_average = round(web.get_data_yahoo('LQD', start=today - timedelta(days=36
 
 BND_12_average = round(web.get_data_yahoo('BND', start=today - timedelta(days=364),end = today)['Close'].sum()/len(web.get_data_yahoo('BND', start=today - timedelta(days=364),end = today)['Close']),2)
 
+#현재가 / 12개월 평균
+TIP_12_average_percent = round(price("tip", today) / TIP_12_average,2)
+DBC_12_average_percent = round(price("dbc", today) / DBC_12_average,2)
+BIL_12_average_percent = round(price("bil", today) / BIL_12_average,2)
+IEF_12_average_percent = round(price("ief", today) / IEF_12_average,2)
+TLT_12_average_percent = round(price("tlt", today) / TLT_12_average,2)
+LQD_12_average_percent = round(price("lqd", today) / LQD_12_average,2)
+BND_12_average_percent = round(price("bnd", today) / BND_12_average,2)
+
 #자산군
 #카나리아 자산군
 canary_universe = [SPY_momentum_score, VEA_momentum_score, VWO_momentum_score, BND_momentum_score ]
 #공격자산
 offensive_universe = [VEA_momentum_score, VWO_momentum_score, BND_momentum_score,QQQ_momentum_score ]
 #안전 자산
-defensive_universe = [TIP_12_average, DBC_12_average, BIL_12_average, IEF_12_average, TLT_12_average, LQD_12_average, BND_12_average]
+defensive_universe = [TIP_12_average_percent, DBC_12_average_percent, BIL_12_average_percent, IEF_12_average_percent, TLT_12_average_percent, LQD_12_average_percent, BND_12_average_percent]
 
 #카나리아 자산군 모멘텀 스코어 확인
 if SPY_momentum_score > 0 and VEA_momentum_score > 0 and VWO_momentum_score >0 and BND_momentum_score > 0:
@@ -88,14 +97,123 @@ if SPY_momentum_score > 0 and VEA_momentum_score > 0 and VWO_momentum_score >0 a
   elif max(offensive_universe) == VWO_momentum_score:
     print(f'VWO - {VWO_momentum_score}')
 else:
-  print('nooo')
+  defensive_universe.sort()
+  biggest = max(defensive_universe)
+  second_biggest = defensive_universe[-2]
+  third_biggest = defensive_universe[-3]
+	
+  print("첫 번째 자산")
+  if biggest == TIP_12_average_percent:
+    if biggest < 1:
+      print("CASH")
+    else:
+      print(f'TIP - {TIP_12_average_percent}')
+  elif biggest == DBC_12_average_percent:
+    if biggest < 1:
+      print("CASH")
+    else:
+      print(f'DBC - {DBC_12_average_percent}')
+  elif biggest == BIL_12_average_percent:
+    if biggest < 1:
+      print("CASH")
+    else:
+      print(f'BIL - {BIL_12_average_percent}')
+  elif biggest == IEF_12_average_percent:
+    if biggest < 1:
+      print("CASH")
+    else:
+      print(f'IEF - {IEF_12_average_percent}')
+  elif biggest == TLT_12_average_percent:
+    if biggest < 1:
+      print("CASH")
+    else:
+      print(f'TLT - {TLT_12_average_percent}')
+  elif biggest == LQD_12_average_percent:
+    if biggest < 1:
+      print("CASH")
+    else:
+      print(f'LQD - {LQD_12_average_percent}')
+  elif biggest == BND_12_average_percent:
+    if biggest < 1:
+      print("CASH")
+    else:
+      print(f'BND - {BND_12_average_percent}')
+	
+  print("2번")
+  if second_biggest == TIP_12_average_percent:
+    if second_biggest < 1:
+      print("CASH")
+    else:
+      print(f'TIP - {TIP_12_average_percent}')
+  elif second_biggest == DBC_12_average_percent:
+    if second_biggest < 1:
+      print("CASH")
+    else:
+      print(f'DBC - {DBC_12_average_percent}')
+  elif second_biggest == BIL_12_average_percent:
+    if second_biggest < 1:
+      print("CASH")
+    else:
+      print(f'BIL - {BIL_12_average_percent}')
+  elif second_biggest == IEF_12_average_percent:
+    if second_biggest < 1:
+      print("CASH")
+    else:
+      print(f'IEF - {IEF_12_average_percent}')
+  elif second_biggest == TLT_12_average_percent:
+    if second_biggest < 1:
+      print("CASH")
+    else:
+      print(f'TLT - {TLT_12_average_percent}')
+  elif second_biggest == LQD_12_average_percent:
+    if second_biggest < 1:
+      print("CASH")
+    else:
+      print(f'LQD - {LQD_12_average_percent}')
+  elif second_biggest == BND_12_average_percent:
+    if second_biggest < 1:
+      print("CASH")
+    else:
+      print(f'BND - {BND_12_average_percent}')
+
+  print('3번')
+  if third_biggest == TIP_12_average_percent:
+    if third_biggest < 1:
+      print("CASH")
+    else:
+      print(f'TIP - {TIP_12_average_percent}')
+  elif third_biggest == DBC_12_average_percent:
+    if third_biggest < 1:
+      print("CASH")
+    else:
+      print(f'DBC - {DBC_12_average_percent}')
+  elif third_biggest == BIL_12_average_percent:
+    if third_biggest < 1:
+      print("CASH")
+    else:
+      print(f'BIL - {BIL_12_average_percent}')
+  elif third_biggest == IEF_12_average_percent:
+    if third_biggest < 1:
+      print("CASH")
+    else:
+      print(f'IEF - {IEF_12_average_percent}')
+  elif third_biggest == TLT_12_average_percent:
+    if third_biggest < 1:
+      print("CASH")
+    else:
+      print(f'TLT - {TLT_12_average_percent}')
+  elif third_biggest == LQD_12_average_percent:
+    if third_biggest < 1:
+      print("CASH")
+    else:
+      print(f'LQD - {LQD_12_average_percent}')
+  elif third_biggest == BND_12_average_percent:
+    if third_biggest < 1:
+      print("CASH")
+    else:
+      print(f'BND - {BND_12_average_percent}')
 
 
-''' 나중에 위로 올리기, defensive universe list도 average_percent로 바꿔야함, 맨 마지막에 모멘텀 스코어 확인 하는 것에 넣기
-TIP_12_average_percent = price("tip", today) / TIP_12_average
 
-defensive_universe.sort()
-a = max(defensive_universe)
-b = defensive_universe[-2]
-c = defensive_universe[-3]
-'''
+
+
